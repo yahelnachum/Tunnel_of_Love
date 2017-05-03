325e069617a218751953faf890f1ce4b56db57a3
Yahel Nachum <yahelnachum@yahoo.com>
2017-05-03 18:37:33 -0400

    fps camera + movement, text serialization, lfs for blend files

---
+++ Unity Project/Assets/Stairs.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stairs : MonoBehaviour {
+
+	int numberOfStairs = 10;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 1; i < numberOfStairs; i++) {
+			GameObject stair = GameObject.CreatePrimitive (PrimitiveType.Cube);
+			stair.transform.position = this.transform.position + new Vector3 (0f, this.transform.localScale.y * i, this.transform.localScale.z * i);
+			stair.transform.localScale = this.transform.localScale;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
